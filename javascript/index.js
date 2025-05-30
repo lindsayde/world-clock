06affe9105be59ed18d85553147c852a83c5deaa
lindsayde <ldehackb@gmail.com>
2025-05-30 09:58:45 -0700

    Added moment.js to file and added Javascript to update real date and time

---
+++ javascript/index.js
@@ -0,0 +1,24 @@
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("MMMM Do, YYYY");
+  londonTimeElement.innerHTML = londonTime.format(
+    "h:m:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do, YYYY");
+  sydneyTimeElement.innerHTML = sydneyTime.format(
+    "h:m:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
